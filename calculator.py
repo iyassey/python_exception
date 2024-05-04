5a6e76364a72b674c573fd0876a21eba4a6cb7dd
Thea <uythea487102@gmail.com>
2024-05-04 17:41:33 +0800

    Pseudocode

---
+++ calculator.py
@@ -0,0 +1,35 @@
+#author__thea_uy
+#date__may_4_2024
+#this program will ask the user for two inputs and an operation
+#this program will also use different exceptions to capture errors during runtime
+
+#create a loop for the whole program 
+
+#create a list that will contain the numbers that the user will input
+
+#create a function that will ask the user for numbers and determine if the the user input float and not string
+
+#create a for loop that will ask the user for two numbers
+
+#create an inner while loop and ask the user for their desire mathematical operations that is limited to addition, subtraction, division, and multiplication
+
+#this block of code will execute if the user chose addition
+
+#this block of code will execute if the user chose subtraction
+
+#this block of code will execute if the user chose multiplication
+
+#this block of code will execute if the user chose division
+
+#this block of code will execute if the user chose an operation that is not included in the choices
+
+#this block of code will execute if the program will detect a zero division error
+
+#ask the user if they want to create a text file
+#if the user wants to create a text file, they can choose if they want to append or write a text file
+
+#exit the loop if the user do not want to create a text file
+
+#ask the user if they want to try again or exit the program
+
+#end of the program 
